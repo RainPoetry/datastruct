65c6bd470603b63ec16703e052dc77537cb5010f
rainpoetry <1060465696@qq.com>
2018-12-20 17:41:23 +0800

    ArrayList

---
+++ src/main/java/com/cc/sourcecode/MyArrayList.java
@@ -0,0 +1,15 @@
+package com.cc.sourcecode;
+
+/**
+ * User: chenchong
+ * Date: 2018/12/20
+ * description:	ArrayList 源码分析
+ *
+ * 		ArrrayList 的所有操作，实际上都是基于数组实现的
+ *
+ * 		remmove：
+ * 			数据移动：
+ * 				System.arraycopy(this.elementData, var1 + 1, this.elementData, var1, var3);
+ */
+public class MyArrayList {
+}
