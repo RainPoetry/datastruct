b99e4d1ef36f693248ff15cf9017aa290285a780
rainpoetry <1060465696@qq.com>
2019-01-18 15:37:32 +0800

    ArrayList

--- src/main/java/com/cc/sourcecode/MyArrayList.java
+++ src/main/java/com/cc/sourcecode/MyArrayList.java
@@ -1,15 +1,43 @@
 package com.cc.sourcecode;
 
+import java.util.Arrays;
+
 /**
  * User: chenchong
  * Date: 2018/12/20
  * description:	ArrayList 源码分析
  *
- * 		ArrrayList 的所有操作，实际上都是基于数组实现的
+ * 		ArrayList 的所有操作，实际上都是基于数组实现的
  *
- * 		remmove：
- * 			数据移动：
- * 				System.arraycopy(this.elementData, var1 + 1, this.elementData, var1, var3);
+ * 		扩容：
+ * 			newCapacity = oldCapacity + (oldCapacity >> 1);
+ * 			elementData = Arrays.copyOf(elementData, newCapacity);
+ *
+ * 		旧数组数据 -> 新的数组：
+ * 			Arrays.copyOf(elementData, newCapacity);
+ *
+ * 		数组内部数据移动(新增、删除操作)：
+ * 			System.arraycopy(this.elementData, var1 + 1, this.elementData, var1, var3);
  */
 public class MyArrayList {
+
+	// 数据的存储方式
+	Object[] elementData;
+
+	/**
+	 * 扩容处理
+	 *
+	 * @param minCapacity
+	 */
+	private void grow(int minCapacity) {
+		// overflow-conscious code
+		int oldCapacity = elementData.length;
+		int newCapacity = oldCapacity + (oldCapacity >> 1);
+		if (newCapacity - minCapacity < 0)
+			newCapacity = minCapacity;
+//		if (newCapacity - MAX_ARRAY_SIZE > 0)
+//			newCapacity = hugeCapacity(minCapacity);
+		// minCapacity is usually close to size, so this is a win:
+		elementData = Arrays.copyOf(elementData, newCapacity);
+	}
 }
