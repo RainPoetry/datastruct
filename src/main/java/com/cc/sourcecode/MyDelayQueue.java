0eb5472eed2aef885c58a2add743a54dd8f7bb51
rainpoetry <1060465696@qq.com>
2019-01-18 15:51:37 +0800

    DelayQueue

---
+++ src/main/java/com/cc/sourcecode/MyDelayQueue.java
@@ -0,0 +1,16 @@
+package com.cc.sourcecode;
+
+/**
+ * User: chenchong
+ * Date: 2019/1/17
+ * description:		DelayQueue 源码分析
+ *
+ * 		特点：
+ * 			1. 无界队列
+ * 			2. 基于 PriorityQueue 实现的 (实际上就是对 PriorityQueue 的 API 简单的封装了一层)
+ * 			3. 通过 ReentrantLock 对 PriorityQueue 的 poll、peek 等操作进行加锁 实现线程安全
+ */
+public class MyDelayQueue {
+
+
+}
