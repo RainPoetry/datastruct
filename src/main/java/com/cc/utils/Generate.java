3983d56c95d35a284cd5a2f1585fabe98553e6df
rainpoetry <1060465696@qq.com>
2019-01-18 15:37:13 +0800

    random utils

---
+++ src/main/java/com/cc/utils/Generate.java
@@ -0,0 +1,20 @@
+package com.cc.utils;
+
+import java.util.Random;
+
+/**
+ * User: chenchong
+ * Date: 2019/1/18
+ * description:
+ */
+public class Generate {
+
+	public static int[] randInt(int size, int bound) {
+		int[] data = new int[size];
+		Random random = new Random();
+		for(int i=0; i < size ;) {
+			data[i++] = random.nextInt(bound);
+		}
+		return data;
+	}
+}
